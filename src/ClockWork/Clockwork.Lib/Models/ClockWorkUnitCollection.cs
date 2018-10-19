d8a524ac98daa49d6c07792bc130152cd47e08ab
Tino Schüttpelz <tino.schuettpelz@systemtechnik-online.de>
2018-10-19 17:05:27 +0200

    initial

---
+++ src/ClockWork/Clockwork.Lib/Models/ClockWorkUnitCollection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Clockwork.Lib.Models
+{
+    public class ClockWorkUnitCollection : ICollection<ClockWorkUnit>
+    {
+        public ClockWorker Worker { get; }
+        private readonly HashSet<ClockWorkUnit> _units = new HashSet<ClockWorkUnit>();
+
+        public ClockWorkUnitCollection(ClockWorker worker, IEnumerable<ClockWorkUnit> units)
+        {
+            Worker = worker;
+            foreach (var clockWorkUnit in units)
+            {
+                _units.Add(clockWorkUnit);
+            }
+        }
+
+        public IEnumerator<ClockWorkUnit> GetEnumerator() => _units.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public void Add(ClockWorkUnit item)
+        {
+            if(item == null) throw new ArgumentNullException(nameof(item));
+
+            var unitsOfInterest = _units.Where(p => p.End >= item.Start || p.Start <= item.End).ToArray();
+            if (!unitsOfInterest.Any()) _units.Add(item);
+            else
+            {
+                foreach (var clockWorkUnit in unitsOfInterest)
+                {
+                    if (clockWorkUnit.Start <= item.Start && clockWorkUnit.End <= item.End) clockWorkUnit.End = item.End;
+                    else if (clockWorkUnit.Start <= item.Start && clockWorkUnit.End >= item.End) return;
+                    else if (clockWorkUnit.Start > item.Start && clockWorkUnit.End >= item.End) clockWorkUnit.Start = item.Start;
+                    else if (clockWorkUnit.Start > item.Start && clockWorkUnit.End < item.End)
+                    {
+                        clockWorkUnit.Start = item.Start;
+                        clockWorkUnit.End = item.End;
+                    }
+                }
+            }
+        }
+
+        public void Clear() => _units.Clear();
+        public bool Contains(ClockWorkUnit item) => _units.Contains(item);
+        public void CopyTo(ClockWorkUnit[] array, int arrayIndex) => _units.CopyTo(array, arrayIndex);
+        public bool Remove(ClockWorkUnit item) => _units.Remove(item);
+
+        public int Count => _units.Count;
+        public bool IsReadOnly => false;
+    }
+}
---
+++ src/ClockWork/Clockwork.Lib/Models/ClockWorker.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clockwork.Lib.Models
+{
+    public class ClockWorker
+    {
+        public string FamilyName { get; set; }
+        public string GivenName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    public class ClockWorkUnit
+    {
+        private readonly IClockWorkCalculator _calculator;
+
+        public ClockWorkUnit(IClockWorkCalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+
+        public CalculationResult CalculateWork()
+        {
+            return _calculator.Calculate(this);
+        }
+    }
+
+    public interface IClockWorkCalculator
+    {
+        CalculationResult Calculate(ClockWorkUnit unit);
+    }
+
+    public class ExactWorkCalculator : IClockWorkCalculator
+    {
+        public CalculationResult Calculate(ClockWorkUnit unit)
+        {
+            return new CalculationResult(unit.End - unit.Start, unit.End - unit.Start);
+        }
+    }
+
+    public class CalculationResult
+    {
+        public CalculationResult(TimeSpan exactDuration, TimeSpan calculatedDuration)
+        {
+            ExactDuration = exactDuration;
+            CalculatedDuration = calculatedDuration;
+        }
+        public TimeSpan ExactDuration { get;  }
+        public TimeSpan CalculatedDuration { get; }
+    }
+}
