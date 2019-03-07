7541114fe4ab8f965a6e2342089e40dc1d200d75
ElPlatero <platero@baelgun.de>
2019-03-07 09:15:48 +0100

    fixed ExactWorkCalculatorTest - was using DateTime.Today, now with fixed date.

--- src/ClockWork/ClockWork.Lib.Tests/ExactWorkCalculatorTests.cs
+++ src/ClockWork/ClockWork.Lib.Tests/ExactWorkCalculatorTests.cs
@@ -11,8 +11,10 @@ namespace ClockWork.Lib.Tests
         [Fact]
         public void Test()
         {
-            var unit = new ClockWorkUnit(DateTime.Today.AddHours(8), DateTime.Today.AddHours(17));
-            var worker = new ClockWorker("Mustermann", "Max", DateTime.Today.AddYears(-21));
+            var startDate = new DateTime(2018, 10, 26).Date;
+
+            var unit = new ClockWorkUnit(startDate.AddHours(8), startDate.AddHours(17));
+            var worker = new ClockWorker("Mustermann", "Max", startDate.AddYears(-21));
 
             var calendar = new ClockWorkUnitCollection(worker, unit);
 
@@ -22,10 +24,10 @@ namespace ClockWork.Lib.Tests
             Assert.Equal(TimeSpan.FromHours(9), calculationResult.CalculatedWorkedHours);
             Assert.Equal(TimeSpan.FromHours(9), calculationResult.ExactWorkedHours);
 
-            var result = calculator.GetBalance(calendar, DateTime.Today.AddDays(1));
+            var result = calculator.GetBalance(calendar, startDate.AddDays(1));
             Assert.Equal(calculationResult.CalculatedWorkedHours, result.CalculatedWorkedHours);
 
-            Assert.Equal("26.10.2018, KW 43: 09:00/08:00", calculationResult.Single().ToString());
+            Assert.Equal("26.10.2018, KW 43: +09:00/+08:00", calculationResult.Single().ToString());
         }
     }
 }
